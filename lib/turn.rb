7d7fefc03d1123526d6f60e4f140a1206b01ebe3
Zach  Smith <zdsmith001@gmail.com>
2019-06-29 13:45:15 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,33 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  (user_input.to_i) - 1
+end
+
+def position_taken?(board, index)
+  if board[index] ==  " " || board[index] == "" || board[index] == nil
+    false
+  else board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, value="X")
+  board[index] = value
+end
