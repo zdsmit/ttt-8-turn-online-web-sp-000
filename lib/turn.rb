b88314b51cd86822e5eabe3fa7d19358ac353647
Zach  Smith <zdsmith001@gmail.com>
2019-06-29 16:12:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   index
-  if valid_move?(board, index)
+  if valid_move?(board, index) && !position_taken?(board, index)
     move(board, index, value="X")
   else
     turn(board)
